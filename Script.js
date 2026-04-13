f7d66a14801178c214dd92402f6f94f837b21c17
lauramarlenelopezgaspar72-a11y <lauramarlenelopezgaspar72@gmail.com>
2026-04-13 07:57:36 -0600

    Nuevoscambios

---
+++ Script.js
@@ -0,0 +1,11 @@
+function mostrarmensaje () {
+    let mensaje = document. getElementById("mensaje");
+
+    mensaje.innerHTML=":cuida y protege a los animales";
+    mensaje.style.display="block";
+    
+    setTimeout(() => {
+        mensaje.style.opacity="1";
+        mensaje.style.transform="translateY(0)";
+        }, 50);
+        }
