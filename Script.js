54584880fdee047e0e6d5d2f7b14f093af3ba75c
lauramarlenelopezgaspar72-a11y <lauramarlenelopezgaspar72@gmail.com>
2026-05-12 11:29:39 -0600

    Actualización segundo parcial

--- Script.js
+++ Script.js
@@ -1,11 +1,26 @@
-function mostrarmensaje () {
-    let mensaje = document. getElementById("mensaje");
+function Mostrar_Mensaje() {
+    let mensaje = document.getElementById("mensaje");
 
-    mensaje.innerHTML=":cuida y protege a los animales";
-    mensaje.style.display= "block";
-    
+    mensaje.innerHTML= "Cuida y protege a los animales";
+    mensaje.style.display = "block";
+
     setTimeout(() => {
-        mensaje.style.opacity="1";
-        mensaje.style.transform="translateY(0)";
-        }, 50);
-        }
+        mensaje.style.opacity ="1";
+        mensaje.style.transform ="translateY(0)";
+    }, 50);
+
+
+}
+function Mostrar_Mensaje2() {
+    let mensaje = document.getElementById("mensaje2");
+
+    mensaje.innerHTML= "Hola bienvenid@ a mi pagina";
+    mensaje.style.display = "block";
+
+    setTimeout(() => {
+        mensaje.style.opacity ="1";
+        mensaje.style.transform ="translateY(0)";
+    }, 50);
+
+
+}
